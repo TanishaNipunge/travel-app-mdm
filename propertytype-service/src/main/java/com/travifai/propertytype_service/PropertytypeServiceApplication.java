004f10dfe762ddfbd1468b0f9f0bb6bd2d317e31
TanishaNipunge <tanishanipunge@gmail.com>
2025-01-18 22:50:56 +0530

    Added city-service, preferences-service, and propertytype-service

---
+++ propertytype-service/src/main/java/com/travifai/propertytype_service/PropertytypeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travifai.propertytype_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertytypeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertytypeServiceApplication.class, args);
+	}
+
+}
---
+++ propertytype-service/src/test/java/com/travifai/propertytype_service/PropertytypeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travifai.propertytype_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertytypeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
